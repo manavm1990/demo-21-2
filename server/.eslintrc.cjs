c4e71f9490462bf14bbd3c65933daa77b9811b51
Manav Misra <manavm@visionwebsoft.com>
2023-03-20 11:44:10 -0500

    Remove GraphQL linting
    Too much trouble - requires Webpack 💩 to import from `.graphql`!

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -4,19 +4,6 @@ module.exports = {
     node: true,
   },
   extends: ["standard", "prettier"],
-  overrides: [
-    {
-      files: ["*.js"],
-      processor: "@graphql-eslint/graphql",
-    },
-    {
-      files: ["*.graphql"],
-      extends: [
-        "plugin:@graphql-eslint/schema-recommended",
-        "plugin:@graphql-eslint/operations-recommended",
-      ],
-    },
-  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
